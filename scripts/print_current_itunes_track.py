ad806b906becca6dae0d8d6443aed9827cda7f54
Christian Long <chlong2@cisco.com>
2018-07-21 23:55:15 -0500

    Docstring

--- scripts/print_current_itunes_track.py
+++ scripts/print_current_itunes_track.py
@@ -1,3 +1,15 @@
+"""
+
+    print_current_itunes_track.py
+
+    Quick demo of the Python-ObjectiveC bridge.
+
+    pip install pyobjc
+
+    https://pythonhosted.org/pyobjc/
+
+"""
+
 import ScriptingBridge
 
 iTunes = ScriptingBridge.SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
